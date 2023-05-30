168982ec47c088040e3b038be57ca96d68d3c2f7
sanjaydell <sanjay.anuchuri@gmail.com>
2023-05-30 22:37:11 +0530

    Created Register Page

--- src/features/Register.jsx
+++ src/features/Register.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Register.css";
 
 function Register() {
+  const [userName, setUsername] = useState("");
   return (
-    <div>Register</div>
-  )
+    <div className="register">
+      <img
+        src="https://cdn.fcglcdn.com/brainbees/images/n/fc-logo-s.jpg"
+        alt="Logo"
+        title="Logo"
+      />
+      <p className="header">
+        <span data-icon="p"></span>
+        Register
+      </p>
+      <div className="formInput">
+        <label className="formLabel">Full Name</label>
+        <input
+          type="text"
+          className="formTextInput"
+          name="userName"
+          id="userName"
+          onChange={(event) => setUsername(event.target.value)}
+          minlength="3"
+          maxlength="30"
+          autocomplete="off"
+          placeholder="Enter your Full Name"
+          value={userName}
+        />
+      </div>
+      <div className="formInput">
+        <label className="formLabel">Mobile Number</label>
+        <div>
+          <div class="mobilePrefix">+91</div>
+          <input
+            type="text"
+            className="mobileInput"
+            placeholder="Enter your Mobile Number"
+            name="usrmb"
+            id="usrmb"
+            value=""
+            maxlength="10"
+            autocomplete="off"
+          />
+          <p className="helperText">
+            OTP will be sent on this Mobile No. for verification.
+          </p>
+        </div>
+      </div>
+        <div className="formInput">
+          <label className="formLabel">Email ID</label>
+          <input
+            type="text"
+            className="formTextInput"
+            placeholder="Enter your Email ID"
+            name="usremail"
+            id="usremail"
+            autocomplete="off"
+            maxlength="50"
+            onchange="javascript: ValidateEmail('usremail','usremailerr');"
+            value=""
+          ></input>
+        </div>
+        <div className="formInput">
+          <label class="formLabel">Password</label>
+          <input
+            type="password"
+            className="formTextInput"
+            placeholder="Enter your Password"
+            name="usrpass"
+            id="usrpass"
+            minlength="8"
+            maxlength="20"
+            autocomplete="off"
+            onchange="validPassword('usrpass','usrpasserr')"
+            value=""
+          />
+          <p className="helperText">
+            Password must be at least 8 characters long with 1 Uppercase, 1
+            Lowercase &amp; 1 Numeric character.
+          </p>
+        </div>
+        <div
+          class="continue"
+          id="continueid"
+          onclick="javascript:return validateForm();"
+        >
+          CONTINUE
+        </div>
+      </div>
+  );
 }
 
-export default Register
+export default Register;
